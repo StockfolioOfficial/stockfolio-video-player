98d34a0191f9fb8a2e026030bde3679a51385f9f
Developer-Mang <chiwon618@gmail.com>
2021-07-30 19:44:28 +0900

    17 feat(repeat): 구간 반복 시작점/종료점 수정 로직 및 구간 반복 이동 로직 개발

--- src/components/RepeatBar.tsx
+++ src/components/RepeatBar.tsx
@@ -7,21 +7,124 @@ interface RepeatBarProps {
 }
 
 function RepeatBar({ videoRef, moveCurrentTime }: RepeatBarProps) {
+  const minTime = 1;
   const [startTime, setStartTime] = useState(0);
-  const [endTime, setEndTime] = useState(4);
+  const [endTime, setEndTime] = useState(0 + minTime);
+  const repeatBarRef = useRef<HTMLDivElement | null>(null);
   const repeatItemRef = useRef<HTMLDivElement | null>(null);
 
   function repeatVideo() {
     if (videoRef === null) return;
     if (videoRef.currentTime < startTime || videoRef.currentTime >= endTime)
       moveCurrentTime(startTime);
+
+    if (!videoRef.paused) window.requestAnimationFrame(repeatVideo);
   }
 
   function changeItemPosition() {
-    if (repeatItemRef.current === null || videoRef === null) return;
+    if (videoRef === null || repeatItemRef.current === null) return;
     const { current: $item } = repeatItemRef;
     $item.style.left = `${(startTime / videoRef.duration) * 100}%`;
     $item.style.width = `${((endTime - startTime) / videoRef.duration) * 100}%`;
+  }
+
+  function mouseDownEdge(
+    e: React.MouseEvent<HTMLDivElement>,
+    target: "start" | "end"
+  ) {
+    if (
+      videoRef === null ||
+      repeatItemRef.current === null ||
+      repeatBarRef.current === null
+    )
+      return;
+    e.stopPropagation();
+    const { clientWidth: barWidth, offsetLeft: barLeft } = repeatBarRef.current;
+    const { clientWidth: itemWidth, offsetLeft: itemLeft } =
+      repeatItemRef.current;
+    const { duration } = videoRef;
+    const minLength = (barWidth * minTime) / duration;
+    let edgePoint =
+      target === "start" ? barLeft + itemLeft : barLeft + itemLeft + itemWidth;
+    const pointerToEdge =
+      target === "start" ? e.pageX - edgePoint : edgePoint - e.pageX;
+    let limitStart = barLeft;
+    let limitEnd = barLeft + barWidth;
+
+    if (target === "start") limitEnd = edgePoint + itemWidth - minLength;
+    else limitStart = edgePoint - itemWidth + minLength;
+
+    function mouseMoveEdge(em: MouseEvent) {
+      let afterEdgePoint =
+        target === "start"
+          ? em.pageX - pointerToEdge
+          : em.pageX + pointerToEdge;
+      if (afterEdgePoint < limitStart) afterEdgePoint = limitStart;
+      if (afterEdgePoint > limitEnd) afterEdgePoint = limitEnd;
+      if (afterEdgePoint === edgePoint) return;
+      edgePoint = afterEdgePoint;
+
+      const changeTime = duration * ((edgePoint - barLeft) / barWidth);
+      if (target === "start") {
+        setStartTime(changeTime);
+      } else {
+        setEndTime(changeTime);
+      }
+      moveCurrentTime(changeTime);
+    }
+
+    function mouseUpEdge() {
+      document.removeEventListener("mousemove", mouseMoveEdge);
+      document.removeEventListener("mouseup", mouseUpEdge);
+    }
+
+    document.addEventListener("mousemove", mouseMoveEdge);
+    document.addEventListener("mouseup", mouseUpEdge);
+  }
+
+  function mouseDownItem(e: React.MouseEvent<HTMLDivElement>) {
+    if (
+      videoRef === null ||
+      repeatItemRef.current === null ||
+      repeatBarRef.current === null
+    )
+      return;
+    const { clientWidth: barWidth, offsetLeft: barLeft } = repeatBarRef.current;
+    const { clientWidth: itemWidth, offsetLeft: itemLeft } =
+      repeatItemRef.current;
+    const { duration } = videoRef;
+    let itemStart = barLeft + itemLeft;
+    let itemEnd = itemStart + itemWidth;
+
+    function mouseMoveItem(em: MouseEvent) {
+      let afterStart = em.pageX - e.nativeEvent.offsetX;
+      let afterEnd = em.pageX - e.nativeEvent.offsetX + itemWidth;
+
+      if (afterStart < barLeft) {
+        afterStart = barLeft;
+        afterEnd = barLeft + itemWidth;
+      }
+      if (afterEnd > barLeft + barWidth) {
+        afterStart = barLeft + barWidth - itemWidth;
+        afterEnd = barLeft + barWidth;
+      }
+
+      if (itemStart === afterStart || itemEnd === afterEnd) return;
+      itemStart = afterStart;
+      itemEnd = afterEnd;
+
+      setStartTime(duration * ((itemStart - barLeft) / barWidth));
+      setEndTime(duration * ((itemEnd - barLeft) / barWidth));
+      moveCurrentTime(duration * ((itemStart - barLeft) / barWidth));
+    }
+
+    function mouseUpItem() {
+      document.removeEventListener("mousemove", mouseMoveItem);
+      document.removeEventListener("mouseup", mouseUpItem);
+    }
+
+    document.addEventListener("mousemove", mouseMoveItem);
+    document.addEventListener("mouseup", mouseUpItem);
   }
 
   useEffect(() => {
@@ -38,42 +141,49 @@ function RepeatBar({ videoRef, moveCurrentTime }: RepeatBarProps) {
 
   useEffect(() => {
     if (videoRef === null || repeatItemRef.current === null) return undefined;
-    repeatVideo();
     changeItemPosition();
     videoRef.addEventListener("timeupdate", repeatVideo);
+    repeatVideo();
     return () => {
       videoRef.removeEventListener("timeupdate", repeatVideo);
-      console.log(repeatVideo);
     };
   }, [startTime, endTime, videoRef, repeatItemRef]);
 
   return (
-    <>
-      <div id="RepeatBar">
-        <div className="RepeatItem" ref={repeatItemRef}>
-          <div className="start" />
-          <div className="end" />
-        </div>
+    <div id="RepeatBar" ref={repeatBarRef}>
+      <div
+        className="RepeatItem"
+        ref={repeatItemRef}
+        onKeyDown={(e) => {
+          console.log(e.code);
+        }}
+        onMouseDown={(e) => mouseDownItem(e)}
+        role="button"
+        aria-label="repeat-item"
+        tabIndex={0}
+      >
+        <div
+          className="start"
+          onKeyDown={(e) => {
+            console.log(e.code);
+          }}
+          onMouseDown={(e) => mouseDownEdge(e, "start")}
+          role="button"
+          aria-label="repeat-starting-point"
+          tabIndex={0}
+        />
+        <div
+          className="end"
+          onKeyDown={(e) => {
+            console.log(e.code);
+          }}
+          onMouseDown={(e) => mouseDownEdge(e, "end")}
+          role="button"
+          aria-label="repeat-ending-point"
+          tabIndex={0}
+        />
       </div>
-      <button type="button" onClick={() => setStartTime(1)}>
-        1초 시작
-      </button>
-      <button type="button" onClick={() => setStartTime(2)}>
-        2초 시작
-      </button>
-      <button type="button" onClick={() => setStartTime(3)}>
-        3초 시작
-      </button>
-      <button type="button" onClick={() => setEndTime(4)}>
-        4초 끝
-      </button>
-      <button type="button" onClick={() => setEndTime(5)}>
-        5초 끝
-      </button>
-      <button type="button" onClick={() => setEndTime(6)}>
-        6초 끝
-      </button>
-    </>
+    </div>
   );
 }
 
